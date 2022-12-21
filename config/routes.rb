af9ab5bec1876b4a48d165dbd83dfe0d9ff96c0b
PraisesPJMT <praisesmusa@outlook.com>
2022-12-21 01:06:57 +0100

    Add `Recipe` & `Public_Recipe` routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "devise/sessions#new"
-  resources :users, only: %i[index show] 
+  resources :users, only: %i[index show] do
+    resources :recipes, only: %i[index show new create destroy] do
+      member do
+        patch :update_public
+      end
+    end
+  end
+
+  resources :public_recipes, only: [:index]
 end
