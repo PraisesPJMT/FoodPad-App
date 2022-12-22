b1f04b4bd0e80e90e97b2495980c2c913ea612f5
PraisesPJMT <praisesmusa@outlook.com>
2022-12-22 12:33:24 +0100

    Add `Recipe Foods` model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal: 0 }
