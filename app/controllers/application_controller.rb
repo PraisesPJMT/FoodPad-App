8fff3356f47405230b77733f7c5fc903a2f99abd
PraisesPJMT <praisesmusa@outlook.com>
2022-12-24 09:33:43 +0100

    Update sign-out route to direct to log-in view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
   end
+
+  private
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
