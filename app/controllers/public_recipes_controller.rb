524f685e4c9659bd755f1324be2e9558418a88f7
PraisesPJMT <praisesmusa@outlook.com>
2022-12-24 07:56:32 +0100

    Adjust view to follow wireframe

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,23 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.all.where(public: true).includes([:user])
+    @ingredients = RecipeFood.all.includes([:food])
+    @ingredients_count = obtain_ingredient_count
+    @ingredients_cost = obtain_ingredient_cost
   end
+
+  private
+
+  def obtain_ingredient_count
+    @ingredients.size
+  end
+
+  def obtain_ingredient_cost
+    cost = 0
+    @ingredients.each do |ingredient|
+      cost += ingredient.food.price.to_i * ingredient.quantity.to_i
+    end
+    cost
+  end
+
 end
